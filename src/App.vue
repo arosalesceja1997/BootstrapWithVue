2a0a143073ca9cc3f99c76e86f7b2b99bc09c55e
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-10-19 17:56:46 -0500

    Anthony - ejemplos

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
+    <b-navbar toggleable="md" type="dark" variant="danger">
+      <b-container>
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        <b-navbar-toggle target="nav_collapse">
+        </b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item :to="{name: 'Home'}" exact>Home</b-nav-item>
+            <b-nav-item :to="{name: 'About'}">About</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .router-link-exact-active {
+    color: white !important;
+  }
 </style>
